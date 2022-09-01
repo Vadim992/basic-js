6bd43bfae7b9070886c4d9bd6c91eab2563ac0c3
Vadim992 <pushtako.vadim867@yandex.ru>
2022-09-02 00:49:45 +0300

    feat: done 18/21

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,8 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  let result = {};
+  let set = new Set();
+  for (let item of domains) {
+    let arr = item.split('.').map(item => '.'+item);
+    arr.reverse();
+    set.add(arr[0]);
+    for (let i = 0; i < arr.length; i++){
+      let k = 0;
+      let sum = '';
+      while (k <= i) {
+        sum += arr[k++];
+      }
+      set.add(sum);
+    }
+  } 
+  for (let value of set) {
+    let num = 0;
+    let str = value.slice(1).split('.').reverse().join('.');
+    for (let item of domains) {
+      item.includes(str) ? num++ : null;
+      
+    }
+    result[value] = num;
+  }
+  return result;
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
